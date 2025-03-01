22e2204d59305569d75e92215f85baf52554bf94
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-01 19:44:06 +0530

    up

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -15,12 +15,15 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
     @NotBlank
     @Size(min = 3,message = "Product name must have atleast 3 characters!")
     private String productName;
+
     @NotBlank
     @Size(min = 6, message = "product name must have atleast 6 characters!")
     private String description;
+
     private String image;
     private Integer quantity;
     private double price; //100
@@ -30,6 +33,10 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 
 
 
