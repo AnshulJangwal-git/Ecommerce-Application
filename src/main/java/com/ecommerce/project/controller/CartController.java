c12c6cbe18f088625fa0bcace354fb02fa85e637
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-07 21:55:45 +0530

    up

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -49,4 +49,28 @@ public class CartController {
         return new ResponseEntity<>(cartDTO, HttpStatus.OK);
     }
 
+    @PutMapping("/cart/products/{productId}/quantity/{operation}")
+    public ResponseEntity<CartDTO> updateCartProduct(@PathVariable Long productId,
+                                                     @PathVariable String operation){
+        CartDTO cartDTO = cartService.updateProductQuantityInCart(productId,
+                operation.equalsIgnoreCase("delete") ? -1 : 1);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
