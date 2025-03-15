b4c90ae364879d9f609e5d88790f37c79550acc2
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-16 00:13:22 +0530

    up

---
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressServiceImpl implements AddressService{
+
+    @Override
+    public AddressDTO createAddress(AddressDTO addressDTO, User user) {
+        return null;
+    }
+}
