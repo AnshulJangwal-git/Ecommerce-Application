772f2fb69157ca5a49dff8a44b2ddceb203313a5
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-16 18:02:58 +0530

    up

---
+++ src/main/java/com/ecommerce/project/payload/OrderItemDTO.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.payload;
+
+public class OrderItemDTO {
+    private Long orderItemId;
+    private ProductDTO product;
+    private Integer quantity;
+    private double discount;
+    private double orderedProductPrice;
+
+}
