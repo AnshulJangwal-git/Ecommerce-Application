0562f9907c76c620b0cd7ef38f1aeaffdc2c378c
AnshulJangwal <anshuljangwal@gmail.com>
2025-03-16 18:03:51 +0530

    up

--- src/main/java/com/ecommerce/project/payload/OrderItemDTO.java
+++ src/main/java/com/ecommerce/project/payload/OrderItemDTO.java
@@ -1,5 +1,12 @@
 package com.ecommerce.project.payload;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemDTO {
     private Long orderItemId;
     private ProductDTO product;
